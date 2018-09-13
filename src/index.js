8cfaaa5c37299b6bafad26419852042f13d5e4a0
zhouhuafei <zhouhuafei@shopex.cn>
2018-09-13 10:38:07 +0800

    回头自己琢磨。别人写的都不靠谱

--- src/index.js
+++ src/index.js
@@ -77,6 +77,7 @@
             });
         },
         drawRoundRect(ctx, x, y, w, h, r) { // 绘制圆角矩形
+            /*
             if (w < 2 * r) {
                 r = w / 2;
             }
@@ -89,6 +90,13 @@
             ctx.arcTo(x + w, y + h, x, y + h, r);
             ctx.arcTo(x, y + h, x, y, r);
             ctx.arcTo(x, y, x + w, y, r);
+            ctx.closePath();
+            */
+            ctx.beginPath();
+            ctx.arc(x + r, y + r, r, Math.PI, Math.PI * 1.5);
+            ctx.arc(x + w - r, y + r, r, Math.PI * 1.5, Math.PI * 2);
+            ctx.arc(x + w - r, y + h - r, r, 0, Math.PI * 0.5);
+            ctx.arc(x + r, y + h - r, r, Math.PI * 0.5, Math.PI);
             ctx.closePath();
         },
     };
